c894c70738531a53246acace18e916fd5431696d
MarcoESCRIBANO <marco.escribano@isen.yncrea.fr>
2022-03-28 12:40:09 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/projet_sonification/MainActivity.java
+++ app/src/main/java/com/example/projet_sonification/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.projet_sonification;
 
+import android.annotation.SuppressLint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SoundPool soundPool;
 
     private AudioManager audioManager;
@@ -29,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
     private int soundId;
 
     private float volume;
+
+    private int streamId;
+
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         // Volumn (0 --> 1)
         //this.volume = currentVolumeIndex / maxVolumeIndex;
 
-        this.volume = Math.sin(10*rotation/(91.189*Math.PI));
+        this.volume = 0;
 
         // Suggests an audio stream whose volume should be changed by
         // the hardware volume controls.
@@ -90,20 +98,85 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Load sound file (destroy.wav) into SoundPool.
-        this.soundId = this.soundPool.load(this, R.raw.stringsound,1);
+        this.soundId = this.soundPool.load(this, R.raw.stringsound1,1);
 
     }
 
 
 
     // When users click on the button
-    public void playSound( )  {
+    public void playSound()  {
         if(loaded)  {
             float leftVolumn = volume;
             float rightVolumn = volume;
-            // Play sound of gunfire. Returns the ID of the new stream.
-            int streamId = this.soundPool.play(this.soundId,leftVolumn, rightVolumn, 1, 1, 1f);
+            // Play sound. Returns the ID of the new stream.
+            streamId = this.soundPool.play(this.soundId,leftVolumn, rightVolumn, 1, 10, 1f);
         }
     }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager =  (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(
+                this,
+                //sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), // version 1 deprecated
+                sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @SuppressLint("LongLogTag")
+    /*public void onSensorChanged(SensorEvent event) {
+        float orientation = (float) Math.toDegrees(event.values[2]);
+        Log.d("TYPE_ORIENTATION",""+orientation);
+
+        //float rotation = getRoll(event.values[3], event.values[0], event.values[1], event.values[2]);
+        //Log.d("TYPE_GAME_ROTATION_VECTOR",""+rotation);
+    }*/
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)) {
+            float[] rotationVector = event.values;
+            float[] rotationMatrix = new float[9];
+            SensorManager.getRotationMatrixFromVector(
+                    rotationMatrix, rotationVector);
+
+            float[] orientation = new float[3];
+
+            SensorManager.getOrientation(rotationMatrix, orientation);
+
+            // Convert radians to degrees
+            long pitch = Math.abs(Math.round(Math.toDegrees(orientation[1])));
+            Log.d("Rotation téléphone",""+pitch);
+            this.volume = (float) Math.sin(10*pitch/(91.19*Math.PI));
+            soundPool.setVolume(streamId, this.volume, this.volume);
+        }
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    /** Get the roll euler angle in radians, which is the rotation around the z axis.
+     * @return the rotation around the z axis in radians (between -PI and +PI) */
+    public float getRollRad (float w, float x, float y, float z) {
+        return (float) Math.atan2(2f * (w * z + y * x), 1f - 2f * (x * x + z * z));
+    }
+
+    /** Get the roll euler angle in degrees, which is the rotation around the z axis. Requires that this quaternion is normalized.
+     * @return the rotation around the z axis in degrees (between -180 and +180) */
+    public float getRoll (float w, float x, float y, float z) {
+        return Math.round((Math.toDegrees(getRollRad( w,  x, y, z)+Math.PI+Math.PI/2))%360);
+    }
+
+
 
 }
