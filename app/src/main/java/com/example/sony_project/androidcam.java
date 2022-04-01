3642d6e955238701b3b4d50d87182d972a13b6aa
MarcoESCRIBANO <marco.escribano@isen.yncrea.fr>
2022-04-01 15:01:49 +0200

    rotation appareil photo

--- app/src/main/java/com/example/sony_project/androidcam.java
+++ app/src/main/java/com/example/sony_project/androidcam.java
@@ -4,6 +4,7 @@ package com.example.sony_project;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
@@ -51,7 +52,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-public class androidcam extends AppCompatActivity implements SensorEventListener {
+public class androidcam extends AppCompatActivity {
 
     private Button btnCapture;
     private TextureView textureView;
@@ -106,7 +107,7 @@ public class androidcam extends AppCompatActivity implements SensorEventListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_androidcam);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         textureView = (TextureView) findViewById(R.id.textureView);
         assert textureView != null ;
         textureView.setSurfaceTextureListener(textureListener);
@@ -149,13 +150,6 @@ public class androidcam extends AppCompatActivity implements SensorEventListener
         CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
         captureBuilder.addTarget(reader.getSurface());
         captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-
-        // Check orientation on Device
-
-        int rotation = ((WindowManager) this.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getOrientation();
-
-        Log.i("imageRotation",""+rotation);
-        captureBuilder.set(CaptureRequest.JPEG_ORIENTATION,ORIENTATIONS.get(rotation));
 
         try{
             file.createNewFile();
@@ -365,70 +359,6 @@ public class androidcam extends AppCompatActivity implements SensorEventListener
         mBackgroundThread.start();
         //mBackgroundThread = new HandlerThread("Camera Background");
         mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
-    }
-
-
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager =  (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorManager.registerListener(
-                this,
-                //sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), // version 1 deprecated
-                sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR),
-                SensorManager.SENSOR_DELAY_FASTEST);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
-    }
-
-    @SuppressLint("LongLogTag")
-    /*public void onSensorChanged(SensorEvent event) {
-        float orientation = (float) Math.toDegrees(event.values[2]);
-        Log.d("TYPE_ORIENTATION",""+orientation);
-
-        //float rotation = getRoll(event.values[3], event.values[0], event.values[1], event.values[2]);
-        //Log.d("TYPE_GAME_ROTATION_VECTOR",""+rotation);
-    }*/
-
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor == sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)) {
-            float[] rotationVector = event.values;
-            float[] rotationMatrix = new float[9];
-            SensorManager.getRotationMatrixFromVector(
-                    rotationMatrix, rotationVector);
-
-            float[] orientation = new float[3];
-
-            SensorManager.getOrientation(rotationMatrix, orientation);
-
-            // Convert radians to degrees
-            long pitch = Math.abs(Math.round(Math.toDegrees(orientation[1])));
-            Log.d("Rotation téléphone",""+pitch);
-            this.volume = (float) Math.sin(10*pitch/(91.19*Math.PI));
-            soundPool.setVolume(streamId, this.volume, this.volume);
-        }
-    }
-
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    /** Get the roll euler angle in radians, which is the rotation around the z axis.
-     * @return the rotation around the z axis in radians (between -PI and +PI) */
-    public float getRollRad (float w, float x, float y, float z) {
-        return (float) Math.atan2(2f * (w * z + y * x), 1f - 2f * (x * x + z * z));
-    }
-
-    /** Get the roll euler angle in degrees, which is the rotation around the z axis. Requires that this quaternion is normalized.
-     * @return the rotation around the z axis in degrees (between -180 and +180) */
-    public float getRoll (float w, float x, float y, float z) {
-        return Math.round((Math.toDegrees(getRollRad( w,  x, y, z)+Math.PI+Math.PI/2))%360);
     }
 }
 
